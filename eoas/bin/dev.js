62eb05a056d71fd71c3f0301f05195ec6777dd37
Axel Marciano <axel.marciano@gmail.com>
2025-01-29 21:47:36 +0100

    fix: bin

--- eoas/bin/dev.js
+++ eoas/bin/dev.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env node_modules/.bin/ts-node
+#!/usr/bin/env node
 // eslint-disable-next-line node/shebang, unicorn/prefer-top-level-await
-;(async () => {
-  const oclif = await import('@oclif/core')
-  await oclif.execute({development: true, dir: __dirname})
-})()
+(async () => {
+  const oclif = await import('@oclif/core');
+  await oclif.execute({ development: true, dir: __dirname });
+})();
