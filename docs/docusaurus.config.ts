80f26ef4e0a9a90ccf4da9b9ab2e124956bfd0a1
Axel Marciano <axel.marciano@gmail.com>
2025-01-30 17:21:25 +0100

    feat: replace social card

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social_card.png',
     navbar: {
       title: 'Expo Open OTA',
       logo: {
