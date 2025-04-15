16f035494f249ea2f2000f5bd154ba3aaeaabe6a
Axel Marciano <axel.marciano@gmail.com>
2025-04-15 04:40:10 +0200

    feat: add picker style & fix missing release channel in prebuild

--- apps/example-app/app/index.tsx
+++ apps/example-app/app/index.tsx
@@ -82,6 +82,7 @@ export default function HomeScreen() {
         </ThemedView>
         <ThemedView>
           <RNPickerSelect
+            style={pickerSelectStyles}
             items={RELEASE_CHANNELS.map(channel => ({
               label: channel,
               value: channel,
@@ -102,6 +103,29 @@ export default function HomeScreen() {
     </SafeAreaView>
   )
 }
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+})
 
 const styles = StyleSheet.create({
   titleContainer: {
