7d882b49d85dfbd3e9ccaaba26f24cffc46f3cfa
Poheart <pohearthk@gmail.com>
2025-07-19 01:58:20 +0800

    feat: adds AWS base endpoint to settings page
    Adds the AWS base endpoint configuration to the settings,
    allowing for more flexible AWS deployments and configurations.

--- internal/handlers/dashboard_handler.go
+++ internal/handlers/dashboard_handler.go
@@ -99,6 +99,7 @@ func GetSettingsHandler(w http.ResponseWriter, r *http.Request) {
 		PUBLIC_LOCAL_EXPO_KEY_PATH:             config.GetEnv("PUBLIC_LOCAL_EXPO_KEY_PATH"),
 		PRIVATE_LOCAL_EXPO_KEY_PATH:            config.GetEnv("PRIVATE_LOCAL_EXPO_KEY_PATH"),
 		AWS_REGION:                             config.GetEnv("AWS_REGION"),
+		AWS_BASE_ENDPOINT:                      config.GetEnv("AWS_BASE_ENDPOINT"),
 		AWS_ACCESS_KEY_ID:                      config.GetEnv("AWS_ACCESS_KEY_ID"),
 		CLOUDFRONT_DOMAIN:                      config.GetEnv("CLOUDFRONT_DOMAIN"),
 		CLOUDFRONT_KEY_PAIR_ID:                 config.GetEnv("CLOUDFRONT_KEY_PAIR_ID"),
